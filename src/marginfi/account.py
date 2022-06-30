58c22fd614cb597f35cc77c5e3d70eb0ede67c5a
Benedict Brady <bqbrady@gmail.com>
2022-06-30 10:46:58 -0500

    /feature switch to json idl

--- src/marginfi/account.py
+++ src/marginfi/account.py
@@ -1,22 +1,32 @@
-from marginfi.schema import MarginfiAccount, MarginfiGroup
+import os
+import json
+from pathlib import Path
+from anchorpy import AccountsCoder, Idl
 from marginfi.solana_rpc import query_solana_account
 
 
-ACCOUNT_DISCRIMINATOR_SIZE = 8
+def load_idl(path=None):
+    if not path:
+        path = os.path.join(os.environ["CURRENT_DIR"], "idls/marginfi.json")
+    with Path(path).open() as f:
+        return json.load(f)
+    
 
-
-def load_margin_account(address, solana_rpc="https://api.mainnet-beta.solana.com"):
+def load_margin_account(address, path_to_idl=None, solana_rpc="https://api.mainnet-beta.solana.com"):
     margin_account = query_solana_account(address, solana_rpc)
-    margin_account = MarginfiAccount.parse(margin_account[ACCOUNT_DISCRIMINATOR_SIZE:])
+    idl = load_idl(path_to_idl)
+    idl = AccountsCoder(Idl.from_json(idl))
+    margin_account = idl.parse(margin_account)
     return margin_account
 
 
-def load_margin_group(address, solana_rpc="https://api.mainnet-beta.solana.com"):
-    margin_account = load_margin_account(address, solana_rpc)
-    margin_group = query_solana_account(margin_account.marginfiGroup, solana_rpc)
-    margin_group = MarginfiGroup.parse(margin_group[ACCOUNT_DISCRIMINATOR_SIZE:])
+def load_margin_group(address, path_to_idl=None, solana_rpc="https://api.mainnet-beta.solana.com"):
+    margin_group = query_solana_account(address, solana_rpc)
+    idl = load_idl(path_to_idl)
+    idl = AccountsCoder(Idl.from_json(idl))
+    margin_group = idl.parse(margin_group)
     return margin_group
 
 
 if __name__ == "__main__":
-    print(load_margin_group("HHS3XAt2UDSr2N6QWfEp5muML4VDggLwX5Tr8xqA6pf3"))
+    print(load_margin_account("HHS3XAt2UDSr2N6QWfEp5muML4VDggLwX5Tr8xqA6pf3"))
